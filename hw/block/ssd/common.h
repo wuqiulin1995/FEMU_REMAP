b0ca19f5bcb63dedc9108d2fae3fc09b5293d0d7
wangke-25 <296574397@qq.com>
2021-01-06 16:40:26 +0800

    file copy

--- hw/block/ssd/common.h
+++ hw/block/ssd/common.h
@@ -56,7 +56,7 @@
 #define STAT_COUNT
 
 #ifdef STAT_COUNT
-#define STAT_OUTPUT_FILE ("/home/nvm/f2fsgc_remap_seg1K_80M_fileserver.csv")
+#define STAT_OUTPUT_FILE ("/home/nvm/copy_remap_seg1K_80M_fileserver.csv")
 #define PRINT_INTERVAL 5	//输出的时间间隔（秒）
 #endif
 
